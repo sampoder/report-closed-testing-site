bfbfc2990cc45cd5878852c9396a6f3193f609ff
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-01-08 18:43:01 +1100

    some mess

--- theme.config.js
+++ theme.config.js
@@ -1,67 +1,48 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/aidenybai/million',
+  docsRepositoryBase: 'https://github.com/millionjs/docs',
+  titleSuffix: ' – Million',
+  feedbackLink: 'Question? Give us feedback →',
+  feedbackLabels: 'feedback',
+  nextLinks: true,
+  prevLinks: true,
+  search: true,
+  darkMode: true,
+  footerText: (
+    <a
+      rel="noopener"
+      className="w-full inline-flex items-center no-underline text-current font-semibold"
+      href="https://vercel.com/?utm_source=millionjs&utm_campaign=oss"
+      target="_blank"
+    >
+      <span className="mr-1">Powered by</span>
+      <span>
+      </span>
+    </a>
+  ),
+  footerEditOnGitHubLink: true, // will link to the docs repo
+  floatTOC: true,
+  unstable_stork: false,
+  unstable_flexsearch: true,
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <img className="md:inline object-contain hidden" style={{ height: '2em' }} src="/icon.svg" />
+      <span className="ml-2 mr-2 font-extrabold hidden md:inline">Million</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        &lt;1kb <span className="italic">compiler-augmented</span> virtual DOM. It's fast!
       </span>
     </>
   ),
   head: (
     <>
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-icon-180x180.png"
-      />
+      <meta name="description" content="<1kb virtual DOM - it's fast!" />
+      <meta name="og:title" content="Million" />
       <link
         rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/android-icon-192x192.png"
+        type="image/svg+xml"
+        href="https://raw.githubusercontent.com/aidenybai/million/main/.github/assets/icon.svg"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/favicon-96x96.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
     </>
   ),
-  search: true,
-  prevLinks: true,
-  nextLinks: true,
-  footer: true,
-  footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
-  unstable_faviconGlyph: '👋',
-}
+};
